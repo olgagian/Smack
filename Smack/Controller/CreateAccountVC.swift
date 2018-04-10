2bf8bf29e5be83030756ee9fa63da05238df128d
olgagian <ioannis@poleiseis.giannakidis.local>
2018-04-10 19:47:01 +0300

    Use of swiftyJSON for register and loggind user

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -33,7 +33,14 @@ class CreateAccountVC: UIViewController {
         guard let pass  = passTxt.text, passTxt.text != "" else {return}
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
-            if success {print("registered user!")}
+            if success {
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in  #imageLiteral(resourceName: "menuProfileIcon")
+                    if success {
+                        print("logged in user!",AuthService.instance.authToke)
+                    }
+                })
+                
+            }
         }
         
     }
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -11,6 +11,7 @@ typealias CompletionHandler = (_ Success:Bool) ->()
 //URL Constants
 let BASE_URL = "https://chattychattest1.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 //Segues
 let TO_LOGIN="toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
@@ -21,6 +22,9 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
-
+//Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
 
